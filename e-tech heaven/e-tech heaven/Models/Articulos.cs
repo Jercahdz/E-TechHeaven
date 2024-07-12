7edc143ba4e4fabb4669b5debbc97a276be56687
Jercahdz <hejohc@gmail.com>
2024-07-11 22:59:39 -0600

    Agregar archivos de proyecto.

---
+++ e-tech heaven/e-tech heaven/Models/Articulos.cs
@@ -0,0 +1,17 @@
+﻿namespace e_tech_heaven.Models
+{
+    public class Articulos
+    {
+        public int ArticuloId { get; set; }
+
+        public string Imagen { get; set; }
+
+        public string Nombre { get; set; }
+
+        public int Precio { get; set; }
+
+        public string Detalles { get; set; }
+
+
+    }
+}
