7350c9cf21ea6c29fde3d557eba2b1c09753f28d
JoseCastillo16 <106782113+JoseCastillo16@users.noreply.github.com>
2024-08-06 22:37:59 -0600

    Creacion de AppDbContext

---
+++ e-tech heaven/e-tech heaven/Controllers/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualBasic;
+using System.Collections.Generic;
+using e_tech_heaven.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace e_tech_heaven.Controllers
+{
+    public class AppDbContext : DbContext
+        {
+            public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+            {
+            }
+            public DbSet<Articulos> ETechHeavenDB { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS01;Database=master;Trusted_Connection=True;TrustServerCertificate=True;");
+            }
+        }
+}
