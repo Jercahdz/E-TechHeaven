4409ce18023913b083800f9841950e43d78c3c3e
JoseCastillo16 <106782113+JoseCastillo16@users.noreply.github.com>
2024-08-06 23:02:59 -0600

    Creacion de la clase Usuario

---
+++ e-tech heaven/e-tech heaven/Models/Usuario.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace e_tech_heaven.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(250)]
+        public string NombreUsuario { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Correo { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Contrasena { get; set; }
+              
+        [Required]
+        [StringLength (10)]
+        public string Estado { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public long Cedula { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public int Telefono { get; set; }
+
+    }
+}
